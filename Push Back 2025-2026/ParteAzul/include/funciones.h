1bdcf1138ebcc6557d873680b4257102b2249590
David Beltrán. <estedavid0104@gmail.com>
2026-03-17 16:41:48 -0500

    Implement autonomous mode for Blue alliance

---
+++ Push Back 2025-2026/ParteAzul/include/funciones.h
@@ -0,0 +1,412 @@
+#include "vex.h"
+#include <iostream>
+
+#include "configuration.h" // Incluimos la configuración de variables
+
+using namespace vex;
+
+using std::cout;
+using std::endl;
+
+//---------------- FUNCIONES DE MOVIMIENTO ----------------
+
+void resetEncoders()
+{
+    LeftMotor1.setPosition(0, rotationUnits::deg);
+    LeftMotor2.setPosition(0, rotationUnits::deg);
+    LeftMotor3.setPosition(0, rotationUnits::deg);
+    LeftMotor4.setPosition(0, rotationUnits::deg);
+    RightMotor1.setPosition(0, rotationUnits::deg);
+    RightMotor2.setPosition(0, rotationUnits::deg);
+    RightMotor3.setPosition(0, rotationUnits::deg);
+    RightMotor4.setPosition(0, rotationUnits::deg);
+}
+
+void setLeftMotors(double speed) { 
+    LeftMotor1.spin(directionType::fwd, speed, velocityUnits::pct);
+    LeftMotor2.spin(directionType::fwd, speed, velocityUnits::pct);
+    LeftMotor3.spin(directionType::fwd, speed, velocityUnits::pct);
+    LeftMotor4.spin(directionType::fwd, speed, velocityUnits::pct);
+} 
+    
+void setRightMotors(double speed) { 
+    RightMotor1.spin(directionType::fwd, speed, velocityUnits::pct);
+    RightMotor2.spin(directionType::fwd, speed, velocityUnits::pct); 
+    RightMotor3.spin(directionType::fwd, speed, velocityUnits::pct); 
+    RightMotor4.spin(directionType::fwd, speed, velocityUnits::pct);
+}
+
+void stopAllMotors()
+{
+    LeftMotor1.stop(brakeType::brake);
+    LeftMotor2.stop(brakeType::brake);
+    LeftMotor3.stop(brakeType::brake);
+    LeftMotor4.stop(brakeType::brake);
+    RightMotor1.stop(brakeType::brake);
+    RightMotor2.stop(brakeType::brake);
+    RightMotor3.stop(brakeType::brake);
+    RightMotor4.stop(brakeType::brake);
+}
+
+void stopRecoleccion()
+{
+    Recoleccion.stop(brakeType::brake);
+}
+
+void recoleccionSubir(int speed, double duration)
+{
+    Recoleccion.spin(directionType::rev, speed, velocityUnits::pct);
+    wait(duration, seconds);
+    stopRecoleccion();
+}
+
+void recoleccionSubirSinTirar(int speed, double duration)
+{
+    Recolector1.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector2.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector3.spin(directionType::rev, 100, velocityUnits::pct);
+    wait(duration, seconds);
+    stopRecoleccion();
+}
+
+void recoleccionSubirStart(int speed)
+{
+    Recoleccion.spin(directionType::rev, speed, velocityUnits::pct);
+}
+
+void recoleccionSubirSinTirarStart(int speed)
+{
+    Recolector1.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector2.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector3.spin(directionType::rev, 100, velocityUnits::pct);
+}
+
+void recoleccionSubirSacarAdelanteStart(int speed)
+{
+    Recolector1.spin(directionType::fwd, speed, velocityUnits::pct);
+    Recolector2.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector3.spin(directionType::rev, 100, velocityUnits::pct);
+}
+
+void recoleccionBajar(int speed, double duration)
+{
+    Recoleccion.spin(directionType::fwd, speed, velocityUnits::pct);
+    wait(duration, seconds);
+    stopRecoleccion();
+}
+
+void recoleccionAlmacenar(int speed, double duration)
+{
+    Recolector1.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector2.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector5.spin(directionType::fwd, speed, velocityUnits::pct);
+    wait(duration, seconds);
+    stopRecoleccion();
+}
+
+void recoleccionBajito(int speed, double duration)
+{
+    Recolector1.spin(directionType::fwd, speed, velocityUnits::pct);
+    Recolector2.spin(directionType::rev, speed, velocityUnits::pct);
+    Recolector3.spin(directionType::rev, 100, velocityUnits::pct);
+    Recolector5.spin(directionType::fwd, speed, velocityUnits::pct);
+    wait(duration, seconds);
+    stopRecoleccion();
+}
+
+
+void moveDistance(double distanceInInches, double speed)
+{
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+    resetEncoders();
+    inertialSensor.resetRotation(); // Asegúrate de que el sensor esté calibrado antes de llamar esto
+
+    // Compensar error de distancia si es necesario
+    distanceInInches = (1.0 - RELATIVE_DISTANCE_ERROR) * distanceInInches;
+
+    //double targetRotations = (distanceInInches / WHEEL_CIRCUMFERENCE) * 360;
+
+    double targetRotations = (distanceInInches / WHEEL_CIRCUMFERENCE) * 360;
+    
+    double initialAngle = inertialSensor.rotation(); // Ángulo inicial
+    double kP = 0.5;                                 // Constante proporcional, puedes ajustar esto
+
+    while (fabs(LeftMotor1.position(rotationUnits::deg)) < targetRotations && fabs(RightMotor1.position(rotationUnits::deg)) < targetRotations)
+    {
+
+        double currentAngle = inertialSensor.rotation();
+        double error = currentAngle - initialAngle; // Positivo = desviado a la derecha
+
+        double correction = error * kP;
+
+        // Ajuste de velocidad con corrección
+        double leftSpeed = speed - correction;
+        double rightSpeed = speed + correction;
+
+        // Limitar velocidades si es necesario
+        leftSpeed = std::max(std::min(leftSpeed, 100.0), -100.0);
+        rightSpeed = std::max(std::min(rightSpeed, 100.0), -100.0);
+
+        setLeftMotors(leftSpeed);
+        setRightMotors(rightSpeed);
+
+        task::sleep(10); // Pequeña pausa para evitar sobrecarga del CPU
+    }
+
+    stopAllMotors();
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+}
+
+void moveDistanceConRecoleccion(double distanceInInches, double speed, int recoSpeed)
+{
+    resetEncoders();
+
+    // Activar recoleccion antes de empezar a moverse
+    recoleccionSubirStart(recoSpeed);
+
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+    resetEncoders();
+    inertialSensor.resetRotation(); // Asegúrate de que el sensor esté calibrado antes de llamar esto
+
+    // Compensar error de distancia si es necesario
+    distanceInInches = (1.0 - RELATIVE_DISTANCE_ERROR) * distanceInInches;
+
+    //double targetRotations = (distanceInInches / WHEEL_CIRCUMFERENCE) * 360;
+
+    double targetRotations = (distanceInInches / WHEEL_CIRCUMFERENCE) * 360;
+    
+    double initialAngle = inertialSensor.rotation(); // Ángulo inicial
+    double kP = 0.5;                                 // Constante proporcional, puedes ajustar esto
+
+    while (fabs(LeftMotor1.position(rotationUnits::deg)) < targetRotations && fabs(RightMotor1.position(rotationUnits::deg)) < targetRotations)
+    {
+
+        double currentAngle = inertialSensor.rotation();
+        double error = currentAngle - initialAngle; // Positivo = desviado a la derecha
+
+        double correction = error * kP;
+
+        // Ajuste de velocidad con corrección
+        double leftSpeed = speed - correction;
+        double rightSpeed = speed + correction;
+
+        // Limitar velocidades si es necesario
+        leftSpeed = std::max(std::min(leftSpeed, 100.0), -100.0);
+        rightSpeed = std::max(std::min(rightSpeed, 100.0), -100.0);
+
+        setLeftMotors(leftSpeed);
+        setRightMotors(rightSpeed);
+
+        task::sleep(10); // Pequeña pausa para evitar sobrecarga del CPU
+    }
+
+    stopAllMotors();
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+}
+
+void moveDistanceConRecoleccionSinTirar(double distanceInInches, double speed, int recoSpeed)
+{
+    resetEncoders();
+
+    // Activar recoleccion antes de empezar a moverse
+    recoleccionSubirSinTirarStart(recoSpeed);
+
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+    resetEncoders();
+    inertialSensor.resetRotation(); // Asegúrate de que el sensor esté calibrado antes de llamar esto
+
+    // Compensar error de distancia si es necesario
+    distanceInInches = (1.0 - RELATIVE_DISTANCE_ERROR) * distanceInInches;
+
+    double targetRotations = (distanceInInches / WHEEL_CIRCUMFERENCE) * 360;
+    
+    double initialAngle = inertialSensor.rotation(); // Ángulo inicial
+    double kP = 0.5;                                 // Constante proporcional, puedes ajustar esto
+
+    while (fabs(LeftMotor1.position(rotationUnits::deg)) < targetRotations && fabs(RightMotor1.position(rotationUnits::deg)) < targetRotations)
+    {
+
+        double currentAngle = inertialSensor.rotation();
+        double error = currentAngle - initialAngle; // Positivo = desviado a la derecha
+
+        double correction = error * kP;
+
+        // Ajuste de velocidad con corrección
+        double leftSpeed = speed - correction;
+        double rightSpeed = speed + correction;
+
+        // Limitar velocidades si es necesario
+        leftSpeed = std::max(std::min(leftSpeed, 100.0), -100.0);
+        rightSpeed = std::max(std::min(rightSpeed, 100.0), -100.0);
+
+        setLeftMotors(leftSpeed);
+        setRightMotors(rightSpeed);
+
+        task::sleep(10); // Pequeña pausa para evitar sobrecarga del CPU
+    }
+
+    stopAllMotors();
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+}
+
+
+void moveDistanceConRecoleccionSacarAdelante(double distanceInInches, double speed, int recoSpeed)
+{
+    resetEncoders();
+
+    // Activar recoleccion antes de empezar a moverse
+    recoleccionSubirSacarAdelanteStart(recoSpeed);
+
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+    resetEncoders();
+    inertialSensor.resetRotation(); // Asegúrate de que el sensor esté calibrado antes de llamar esto
+
+    // Compensar error de distancia si es necesario
+    distanceInInches = (1.0 - RELATIVE_DISTANCE_ERROR) * distanceInInches;
+
+    double targetRotations = (distanceInInches / WHEEL_CIRCUMFERENCE) * 360;
+    
+    double initialAngle = inertialSensor.rotation(); // Ángulo inicial
+    double kP = 0.5;                                 // Constante proporcional, puedes ajustar esto
+
+    while (fabs(LeftMotor1.position(rotationUnits::deg)) < targetRotations && fabs(RightMotor1.position(rotationUnits::deg)) < targetRotations)
+    {
+
+        double currentAngle = inertialSensor.rotation();
+        double error = currentAngle - initialAngle; // Positivo = desviado a la derecha
+
+        double correction = error * kP;
+
+        // Ajuste de velocidad con corrección
+        double leftSpeed = speed - correction;
+        double rightSpeed = speed + correction;
+
+        // Limitar velocidades si es necesario
+        leftSpeed = std::max(std::min(leftSpeed, 100.0), -100.0);
+        rightSpeed = std::max(std::min(rightSpeed, 100.0), -100.0);
+
+        setLeftMotors(leftSpeed);
+        setRightMotors(rightSpeed);
+
+        task::sleep(10); // Pequeña pausa para evitar sobrecarga del CPU
+    }
+
+    stopAllMotors();
+    wait(100, msec); // Espera breve para asegurar que se reinicie
+}
+
+double computerPID(PID &pid, double setpoint, double current, double dt)
+{
+    pid.error = setpoint - current;
+    pid.Integral += pid.error * dt;
+
+    double derivative = (pid.error - pid.prevError) / dt;
+    double output = pid.kp * pid.error + pid.ki * pid.Integral + pid.kd * derivative;
+    pid.prevError = pid.error;
+    return output;
+}
+
+void resetPID(PID &pid)
+{
+    pid.error = 0;
+    pid.prevError = 0;
+    pid.Integral = 0;
+}
+
+void calibrateInertial()
+{
+    inertialSensor.calibrate();
+    while (inertialSensor.isCalibrating()){
+        wait(100, msec);
+    }
+}
+
+double normalizeAngle(double angle)
+{
+    while (angle > 180)
+        angle -= 360;
+    while (angle < -180)
+        angle += 360;
+    return angle;
+}
+
+void rotateOnAxis(double targetAngle, double maxSpeed, PID &pid_t)
+{
+    resetPID(pid_t);
+
+    double lastTime = Brain.timer(msec);
+
+    while (true)
+    {
+        double currentAngle = inertialSensor.rotation(degrees);
+
+        // Error normalizado (camino más corto)
+        double error = normalizeAngle(targetAngle - currentAngle);
+
+        if (fabs(error) < 2) break;
+
+        double currentTime = Brain.timer(msec);
+        double dt = (currentTime - lastTime) / 1000.0;
+        lastTime = currentTime;
+
+        double power = pid_t.kp * error
+                     + pid_t.kd * ((error - pid_t.prevError) / dt);
+
+        pid_t.prevError = error;
+
+        if (power > maxSpeed)
+            power = maxSpeed;
+        if (power < -maxSpeed)
+            power = -maxSpeed;
+
+        setLeftMotors(power);
+        setRightMotors(-power);
+
+        task::sleep(10);
+    }
+
+    stopAllMotors();
+    wait(100, msec);
+}
+
+void rotateOnAxisRecoleccion(double targetAngle, double maxSpeed, PID &pid_t)
+{
+    resetPID(pid_t);
+
+    // Activar recolección mientras se mueve
+    recoleccionSubirStart(80);
+
+    double lastTime = Brain.timer(msec);
+
+    while (true)
+    {
+        double currentAngle = inertialSensor.rotation(degrees);
+
+        // Error normalizado (camino más corto)
+        double error = normalizeAngle(targetAngle - currentAngle);
+
+        if (fabs(error) < 2) break;
+
+        double currentTime = Brain.timer(msec);
+        double dt = (currentTime - lastTime) / 1000.0;
+        lastTime = currentTime;
+
+        double power = pid_t.kp * error
+                     + pid_t.kd * ((error - pid_t.prevError) / dt);
+
+        pid_t.prevError = error;
+
+        if (power > maxSpeed)
+            power = maxSpeed;
+        if (power < -maxSpeed)
+            power = -maxSpeed;
+
+        setLeftMotors(power);
+        setRightMotors(-power);
+
+        task::sleep(10);
+    }
+
+    stopAllMotors();
+    wait(100, msec);
+}
---
+++ Push Back 2025-2026/ParteAzul/src/main.cpp
@@ -0,0 +1,220 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       PC                                                        */
+/*    Created:      2/6/2026, 5:09:59 PM                                      */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+#include "configuration.h"
+#include "driver.h"
+#include "funciones.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+brain Brain;
+controller Controller;
+inertial inertialSensor = inertial(PORT4);
+
+// Motores del lado izquierdo (puertos 1-4)
+motor LeftMotor1(PORT5, false);
+motor LeftMotor2(PORT6, true);
+motor LeftMotor3(PORT7, false);
+motor LeftMotor4(PORT8, true);
+motor_group LeftDrive(LeftMotor1, LeftMotor2, LeftMotor3, LeftMotor4);
+
+// Motores del lado derecho (puertos 7-10)
+motor RightMotor1(PORT14, true);
+motor RightMotor2(PORT15, false);
+motor RightMotor3(PORT11, true);
+motor RightMotor4(PORT16, false);
+motor_group RightDrive(RightMotor1, RightMotor2, RightMotor3, RightMotor4);
+
+motor Recolector1(PORT3, true);
+motor Recolector2(PORT18, true);
+motor Recolector3(PORT2, false);
+motor Recolector4(PORT13, true);
+motor Recolector5(PORT12, false);
+motor_group Recoleccion(Recolector1, Recolector2, Recolector3, Recolector4, Recolector5);
+
+vex::pneumatics BarraSacar(Brain.ThreeWirePort.A);
+vex::pneumatics BarraLoader(Brain.ThreeWirePort.B);
+
+bool pistonLoader = true;
+bool pistonSacar = true;
+
+// Var PID
+PID pid;
+
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void)
+{
+
+    // All activities that occur before the competition starts
+    // Example: clearing encoders, setting servo positions, ...
+    inertialSensor.calibrate();
+    while (inertialSensor.isCalibrating()) {
+    vex::task::sleep(100); // Espera hasta que la calibración esté completa
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void)
+{
+    pid.kp = 0.7;
+    pid.kd = 0.25;
+    pid.ki = 0.0009;
+/*
+    // PASO 1: Movimiento recto, girar y avanzar hacia el Goal
+    moveDistance(45, 85); // Mover hacia adelante 14 pulgadas a velocidad 80%
+    rotateOnAxis(-93 , 85, pid); // Girar 90 grados a la derecha velocidad 80%
+    moveDistance(26, -40);
+    recoleccionSubir(100, 1.5); // Activar recolección durante 3 segundos a velocidad 100%
+    
+    // PASO 2: Poner el bloque y volver al Loader
+    moveDistance(27, 20);
+    BarraLoader.open();
+    rotateOnAxis(-4, 20, pid); // Girar 90 grados a la izquierda a velocidad 80%
+    
+    moveDistance(9, 100); // Mover hacia adelante 14 pulgadas a velocidad 80%
+    moveDistanceConRecoleccionSinTirar(5.5, 100, 70); // Mover hacia adelante 14 pulgadas a velocidad 80% con recolección activa
+    recoleccionAlmacenar(60, 2); // Activar recolección durante 3 segundos a velocidad 100%
+*/    
+    moveDistance(5, -20); // Mover hacia atrás 14 pulgadas a velocidad 80%
+    BarraLoader.close();
+    moveDistance(50, -20); // Mover hacia atrás 14 pulgadas a velocidad 80%
+    recoleccionSubir(100, 6); // Activar recolección durante 3 segundos a velocidad 100%
+
+}
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void)
+{
+    // User control code here, inside the loop
+    while (1)
+    {
+        twoJoysticksControl();
+        // Control del motor recolector y rampa usando L1 y L2
+        // Mover recoleccion completa para recolectar
+        if (Controller.ButtonL2.pressing())
+        {
+            Recoleccion.spin(directionType::rev, 100, velocityUnits::pct);
+            // Mover recoleccion completa para devolver
+        }
+        else if (Controller.ButtonL1.pressing())
+        {
+            Recoleccion.spin(directionType::fwd, 100, velocityUnits::pct);
+        }
+        // Mover recoleccion para Goal bajito
+        else if (Controller.ButtonR1.pressing())
+        {
+            Recolector1.spin(directionType::fwd, 100, velocityUnits::pct);
+            Recolector2.spin(directionType::rev, 100, velocityUnits::pct);
+            Recolector3.spin(directionType::rev, 100, velocityUnits::pct);
+            Recolector5.spin(directionType::fwd, 100, velocityUnits::pct);
+        }
+        // Mover recoleccion para almacenar
+        else if (Controller.ButtonR2.pressing())
+        {
+            Recolector1.spin(directionType::rev, 100, velocityUnits::pct);
+            Recolector2.spin(directionType::rev, 100, velocityUnits::pct);
+            Recolector3.spin(directionType::rev, 100, velocityUnits::pct);
+            Recolector5.spin(directionType::fwd, 100, velocityUnits::pct);
+        }
+        else
+        {
+            Recoleccion.stop(brakeType::hold);
+        }
+
+        if (Controller.ButtonB.pressing())
+        {
+            // Esperamos a que el botón sea liberado para evitar múltiples activaciones en una sola pulsación
+            while (Controller.ButtonB.pressing()){
+                // Espera a que el botón se suelte
+            }
+
+            // Cambiamos el estado del pistón
+            pistonLoader = !pistonLoader;
+
+            // Ejecutamos la acción correspondiente Abrir o Cerrar segun en que este
+            if (pistonLoader){
+                BarraLoader.open();
+            }
+            else{
+                BarraLoader.close();
+            }
+        }
+
+        if (Controller.ButtonDown.pressing())
+        {
+            // Esperamos a que el botón sea liberado para evitar múltiples activaciones en una sola pulsación
+            while (Controller.ButtonDown.pressing()){
+                // Espera a que el botón se suelte
+            }
+
+            // Cambiamos el estado del pistón
+            pistonSacar = !pistonSacar;
+
+            // Ejecutamos la acción correspondiente Abrir o Cerrar segun en que este
+            if (pistonSacar){
+                BarraSacar.open();
+            }
+            else{
+                BarraSacar.close();
+            }
+        }
+
+        // Espera para evitar saturar el CPU
+        task::sleep(20);
+    }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main()
+{
+    // Set up callbacks for autonomous and driver control periods.
+    Competition.autonomous(autonomous);
+    Competition.drivercontrol(usercontrol);
+
+    // Run the pre-autonomous function.
+    pre_auton();
+
+    // Prevent main from exiting with an infinite loop.
+    while (true)
+    {
+        wait(100, msec);
+    }
+}
